3651b2b3ca5e2a42f101f13134fd4edd1c665162
Shilin Victor <chrono.monochrome@gmail.com>
2018-01-11 21:20:33 +0300

    Remove dups from system/hardware
    Change-Id: I369f3bad13bb26a5f69309c170db57b49e0856ae

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,7 @@
 source system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "android.system:system/hardware/interfaces" \
-  "android.hidl:system/libhidl/transport"
+    "android.frameworks:frameworks/hardware/interfaces" \
+    "android.hardware:hardware/interfaces" \
+    "android.hidl:system/libhidl/transport"
 
